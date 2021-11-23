5d2c635889f1bbc9450b1592cce06918028f7660
Daritza Teheran <daritzat@uninorte.edu.co>
2021-11-23 15:03:37 -0500

    Primer login - Seb

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Index.html')
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8888)
