d9b43ec5040bb3cb627f188e3df594e594956503
Diana <dianacarolina0329@gmail.com>
2021-11-28 01:26:10 -0500

    intento de insert

--- main.py
+++ main.py
@@ -52,7 +52,25 @@ def admin():
     else:
         return render_template('Plogin.html')
 
+@app.route('/admin/register', methods=['POST', 'GET'])
+def admin_reg():    
 
+    #if render_template('Reghelper.html') == True: 
+
+        if request.method == 'POST':
+            name = request.form['name']
+            Lname = request.form['Lname']
+            CC = request.form['CC']
+            pw = request.form['password']
+            user = request.form['user']
+            rol1 = request.form['member']
+            rol2 = request.args.get("member")
+            
+            print(name, Lname, CC, pw, user, rol1, rol2)
+        else:
+            return render_template('Reghelper.html')
+    #else:
+    #   return render_template('Plogin.html')
 #--------------------------------------Módulo de médicos-----------------------------------------------------
 
 @app.route('/medico', methods=['POST', 'GET'])
@@ -101,14 +119,15 @@ def ayudante():
         if len(myresult) == 0:
             return render_template('Alogin.html')
         else:
-           return 'Modulo de ayudantes' 
+           return render_template('Reghelper.html')
     else:
         return render_template('Alogin.html')
+       
 
 
-"""@app.route('/admin', methods=['POST', 'GET'])
-def Registro_Gestion():
-    return 'Registro de caso o Gestión'"""
+#@app.route('/admin', methods=['POST', 'GET'])
+#def Registro_Gestion():
+   # return 'Registro de caso o Gestión'"""
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8888)
