f076f74e3d29e3b3055182eac0e3d00a66ddc8ff
Daritza Teheran <daritzat@uninorte.edu.co>
2021-11-29 08:34:32 -0500

    ayudante

--- static/js/delete.js
+++ static/js/delete.js
@@ -34,3 +34,15 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
     });
+
+let Checked = null;
+//The class name can vary
+for (let CheckBox of document.getElementsByClassName('only-one')){
+	CheckBox.onclick = function(){
+  	if(Checked!=null){
+      Checked.checked = false;
+      Checked = CheckBox;
+    }
+    Checked = CheckBox;
+  }
+}
